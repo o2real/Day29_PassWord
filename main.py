80854a216d693e1c164d8620130d967ca88b87c2
Kingcode <136673119+o2real@users.noreply.github.com>
2024-08-21 16:10:19 +0900

    password manager

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -30,21 +31,26 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="please make sure you haven't left any fields empty.")
     else:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+            data.update(new_data)
 
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
-                                                      f"\npassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} / {email} / {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
-        else:
+        with open("data.json", "w") as data_file:
+            json.dump(data, data_file, indent=4)
+
             website_entry.delete(0, END)
             password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
